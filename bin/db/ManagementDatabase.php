8ef96d9597de179b1d8a452b4d774acf9c810a6c
Stepan <stepan.rulyov@gmail.com>
2024-01-08 00:42:03 +0300

    фиксы

--- bin/db/ManagementDatabase.php
+++ bin/db/ManagementDatabase.php
@@ -9,11 +9,12 @@ abstract class ManagementDatabase
     private static string $username = "b32_35715263";
     private static string $password = "C9rKK3Zyef1KGEH";
     private static string $hostname = "sql312.byethost32.com";
+    private static string $database = "b32_35715263_pnotltd";
     private static object $connection;
 
-    private static function connect()
+    private static function connect(): void
     {
-        self::$connection = new mysqli(self::$hostname, self::$username, self::$password);
+        self::$connection = new mysqli(self::$hostname, self::$username, self::$password, self::$database, 3306);
 
         if (self::$connection->connect_error)
         {
