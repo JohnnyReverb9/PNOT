6c4da2534f30b00a88c153517a64335f5ee3e051
Stepan <stepan.rulyov@gmail.com>
2024-01-09 00:28:32 +0300

    Переход на новую версию

--- src/footer.js
+++ src/footer.js
@@ -1,2 +1,2 @@
 document.getElementById("current-year").innerHTML = new Date().getFullYear();
-document.getElementById("footer-version").innerHTML = "v0.0.2";
+document.getElementById("footer-version").innerHTML = "v0.1.1";
