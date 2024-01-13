861ea04c262569b7df339959bf25332cd727dd8e
Stepan <stepan.rulyov@gmail.com>
2024-01-13 21:14:05 +0300

    Главная страница

--- src/footer.js
+++ src/footer.js
@@ -1,2 +1,2 @@
 document.getElementById("current-year").innerHTML = new Date().getFullYear();
-document.getElementById("footer-version").innerHTML = "v0.1.1";
+document.getElementById("footer-version").innerHTML = "v1.1";
